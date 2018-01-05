9588896cad4f289b77e4f9602c138add730dc57d
jummixe <jummixe@gmail.com>
2018-01-05 19:15:32 +0300

    Nice commit message

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+﻿# -*- coding: utf-8 -*-
 import os
 import sys
 import json
@@ -40,7 +41,7 @@ def webhook():
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
 
-                    send_message(sender_id, "roger that!")
+                    send_message(sender_id, "Битва!".decode('UTF-8'))
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass

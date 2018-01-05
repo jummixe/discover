94bc731d3ae23e80925af9f6803797286746c112
jummixe <jummixe@gmail.com>
2018-01-05 19:54:17 +0300

    no message

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-                    send_message(sender_id, "Куку".encode('utf-8'))
+                    send_message(sender_id, "Куку".encode('unicode_escape'))
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass

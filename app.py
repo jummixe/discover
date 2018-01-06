2c6912a71a80a33fd3dc7b1e73033427d139d0fb
jummixe <jummixe@gmail.com>
2018-01-07 00:23:49 +0300

    no message

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-                    if message_text.decode('UTF-8') == "Аніме".decode('UTF-8'):
+                    if message_text == "Аніме".decode('UTF-8'):
                         send_message(sender_id, "Куку")
                     else:
                         send_message(sender_id, "Брррр")

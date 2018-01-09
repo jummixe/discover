bd720abecad5bab706953e1c121c0e0f69a521ea
jummixe <jummixe@gmail.com>
2018-01-09 22:16:58 +0300

    no message

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ class Products(db.Model):
 
 
 #Function executed before first request on the server.
-@app.before_first_request(send_automatic)
+@app.before_first_request(send_automatic())
 def send_automatic():
     print('exec')
     def run_sender():

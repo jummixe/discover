fe096605b88b6d04a68c8317bd4e59157499d0b4
jummixe <jummixe@gmail.com>
2018-01-09 21:04:23 +0300

    no message

--- app.py
+++ app.py
@@ -104,8 +104,7 @@ def verify():
     return "Hello world", 200
 x=1
 
-#Function executed before first request on the server.
-@app.before_first_request(send_automatic())
+
 def send_automatic():
     def run_sender():
             send_message(u'1579846222104780', '<3')
@@ -113,6 +112,9 @@ def send_automatic():
 
     thread = threading.Thread(target=run_sender)
     thread.start()
+
+#Function executed before first request on the server.
+app.before_first_request(send_automatic)
 
 @app.route('/', methods=['POST'])
 def webhook():

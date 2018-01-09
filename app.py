f569d48d86e9e90875894b2f3f3fd5b6cd3b51e4
jummixe <jummixe@gmail.com>
2018-01-09 20:47:35 +0300

    no message

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["SQLALCHEMY_DATABASE_URI"]
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["DATABASE_URL"]
 
 db = SQLAlchemy(app)
 #configurating  Discover

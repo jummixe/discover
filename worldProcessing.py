2c827c38c6591008f64c904c804417a51b001fb1
jummixe <jummixe@gmail.com>
2018-09-30 20:54:31 +0300

    no message

--- worldProcessing.py
+++ worldProcessing.py
@@ -11,7 +11,7 @@
 import discover as discoverWorld
 
 def return_thoughts():
-    return discover.rethink_events()
+    return discoverWorld.rethink_events()
 
 def init_discover():
     global discoverChar

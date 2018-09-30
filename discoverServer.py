ff333b4f6a0d5863b06e6573a2905141fe24a924
jummixe <jummixe@gmail.com>
2018-09-30 21:23:02 +0300

    no message

--- discoverServer.py
+++ discoverServer.py
@@ -115,10 +115,11 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-                    if message_text == "Аніме".decode('UTF-8'):
-                        send_message(sender_id, sender_id)
-                    else:
-                        send_message(sender_id,discover.return_thoughts())
+                    if sender_id != recipient_id:
+                        if message_text == "Аніме".decode('UTF-8'):
+                            send_message(sender_id, sender_id)
+                        else:
+                            send_message(sender_id,WorldProcessing.return_thoughts())
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
 
